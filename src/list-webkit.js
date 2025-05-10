26b75a4664b9b7fa681d5bf7e86afd4046646585
unknown <regime_skylark.0y@icloud.com>
2025-05-10 19:56:14 -0400

    🛠️FIXED🛠️ up code structure and the like

--- src/list-webkit.js
+++ src/list-webkit.js
@@ -1,20 +1,30 @@
-var appName = $params
+var browser = $params
 
-var app    = Application(appName)
+var app    = Application(browser)
 app.includeStandardAdditions = true
-var items  = []
-var titles = app.windows.tabs.name()
-var urls   = app.windows.tabs.url()
 
-for (var w = 0; w < titles.length; w++) {
-  for (var t = 0; t < titles[w].length; t++) {
+// Walk through each
+var items  = []
+var windows = app.windows;
+
+for (var window_index = 0; window_index < windows.length; window_index++) {
+
+  var window = windows[window_index]
+  var tabs = window.tabs || []; // Fallback if empty
+
+  for (var tab_index = 0; tab_index < tabs.length; tab_index++) {
+    var current_tab = tabs[tab_index];
+
+    var url   = current_tab.url()   || '';
+    let matchUrl = url.replace(/(^\w+:|^)\/\//, "");
+    let title = current_tab.name() || matchUrl;
     items.push({
-      title:       titles[w][t] || '',
-      url:         urls[w][t]   || '',
-      subtitle:    urls[w][t]   || '',
-      windowIndex: w || '',
-      tabIndex:    t || '',
-      arg:         JSON.stringify([w, t])
+      title:       title || '',
+      url:         url || '',
+      subtitle:    url || '',
+      windowIndex: window_index || '',
+      tabIndex:    tab_index || '',
+      arg:         JSON.stringify([window_index, tab_index])
     })
   }
 }
